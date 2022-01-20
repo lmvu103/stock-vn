de73a504ec8879e7136a99d088a0b27bdafad0d4
lmvu103 <lmvu103@gmail.com>
2022-01-20 22:32:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"lmvu103@gmail.com\"\n\
+echo "
+[general]\n
+email = \"lmvu103@gmail.com\"\n
 " > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "
+[server]\n
+headless = true\n
+enableCORS=false\n
+port = $PORT\n
 " > ~/.streamlit/config.toml
